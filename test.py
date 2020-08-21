eafa160a65dad049f44f076ea9e568a1cc922fb4
Sihan Cao <sihancao496@gmail.com>
2020-08-20 22:45:59 -0400

    add how to save and an example

--- test.py
+++ test.py
@@ -76,3 +76,7 @@ data1, data2, data3, data4 = np.random.randn(100, 4)
 fig, (ax1, ax2) = plt.subplots(1, 2)
 my_plotter(ax1, data1, data2, {'marker': 'x'})
 my_plotter(ax2, data3, data4, {'marker': 'o'})
+
+# Save image
+# plt.savefig('filename')
+plt.savefig('test')
